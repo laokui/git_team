6836e05032a6358cadb7a0e1d4fc89fbef0c812b
lmk <1475239605@qq.com>
2021-03-03 20:25:49 +0800

    第2次提交

---
+++ git_test/src/com/java/test/Test2.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+/**
+ * @ClassName Test2
+ * @Description:
+ * @Author :lmk
+ * @Date 2021/3/3
+ * @Version V1.0
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
