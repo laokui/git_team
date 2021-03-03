ef06b44346c35d697ad4868eee22532071e32b83
lmk <1475239605@qq.com>
2021-03-03 20:22:15 +0800

    第一次提交

---
+++ git_test/src/com/java/test/Test.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+/**
+ * @ClassName Test
+ * @Description:
+ * @Author :lmk
+ * @Date 2021/3/3
+ * @Version V1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git Test!");
+    }
+}
